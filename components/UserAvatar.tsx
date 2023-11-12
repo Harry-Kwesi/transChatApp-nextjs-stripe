6b96044789acfc5727786547e1d6fd0aecc29bef
Harry-Kwesi <harrydegraft@hotmail.com>
2023-11-12 23:36:30 +0530

    fix issue with avatal fallback

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,24 +1,29 @@
-import { cn } from "@/lib/utils"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import Image from 'next/image'
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
 
-function UserAvatar({name, image, className}:{name:string, image: string, className:string}) {
+function UserAvatar({
+  name,
+  image,
+  className,
+}: {
+  name: string;
+  image: string;
+  className: string;
+}) {
   return (
-<Avatar className={cn("bg-white text-black", className)}>
-    {image && (
-    <Image
-      src={image}
-      width={500}
-      height={500}
-      alt={name}
-      className=""
-    />)}
-    <AvatarFallback delayMs={1000} className="dark:bg-white dark:text-black text-lg">
-    { name?.match(/\b\w/g)?.join("")}
-    </AvatarFallback>
-</Avatar>
-
-  )
+    <Avatar className={cn("bg-white text-black", className)}>
+      {image && (
+        <Image src={image} width={500} height={500} alt={name} className="" />
+      )}
+      <AvatarFallback
+        delayMs={1000}
+        className="dark:bg-white dark:text-black text-lg"
+      >
+        {name?.match(/\b\w/g)?.join("")}
+      </AvatarFallback>
+    </Avatar>
+  );
 }
 
-export default UserAvatar
+export default UserAvatar;
