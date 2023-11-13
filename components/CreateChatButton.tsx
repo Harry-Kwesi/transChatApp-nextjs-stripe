c766c14b6e1629ef7c89b1e885ccb872d848d1b9
Harry-Kwesi <harrydegraft@hotmail.com>
2023-11-13 20:14:11 +0530

    added chat button and homepage

---
+++ components/CreateChatButton.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { Button } from './ui/button'
+import { Ghost, MessageSquarePlusIcon } from "lucide-react";
+
+function CreateChatButton() {
+
+    const router = useRouter();
+
+    const createNewChat = async () => {
+        
+    }
+    
+  return (
+    <Button onClick={createNewChat} variant={"ghost"}><MessageSquarePlusIcon/></Button>
+  )
+}
+
+export default CreateChatButton
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,23 @@ import UserButton from "./UserButton";
 import { authOptions } from "@/auth";
 import Link from "next/link";
 import { MessagesSquareIcon } from "lucide-react";
+import CreateChatButton from "./CreateChatButton";
 
 const Header = async () => {
   const session = await getServerSession(authOptions);
   console.log("url", session);
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
-      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+      <nav className="flex flex-col sm:flex-row items-center  p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
-        <div className="flex-1 flex items-center justify-end space-x-4"></div>
+        <div className="flex-1 flex items-center justify-end space-x-4">
 
         {session ? (
           <>
             <Link href={"/chat"} prefetch={false}>
            <MessagesSquareIcon className="text-black dark:text-white"/>
             </Link>
+            <CreateChatButton/>
           </>
         ) : (
           <Link href='/pricing' prefetch={false}>
@@ -28,7 +30,9 @@ const Header = async () => {
         )}
         <DarkModeToggle />
         <UserButton  session={session}/>
+        </div>
       </nav>
+     
     </header>
   );
 };
