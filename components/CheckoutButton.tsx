3ec4582ef6cc66576c137e5fb130e47c1192f445
Harry-Kwesi <harrydegraft@hotmail.com>
2023-11-17 23:40:13 +0530

    quick changes on the checkout button

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CheckoutButton() {
   return (
-    <div>CheckoutButton</div>
+   <button className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible-outline focus-visible-outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80">Sign Up</button>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,6 @@ import CreateChatButton from "./CreateChatButton";
 
 const Header = async () => {
   const session = await getServerSession(authOptions);
-  console.log("url", session);
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center  p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
