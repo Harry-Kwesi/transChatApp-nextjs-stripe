ce527eabe12a44a71cdf5a49992771b566ff2709
Harry-Kwesi <harrydegraft@hotmail.com>
2024-02-06 17:31:35 +0530

    set up products for stripe

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,17 +1,53 @@
 'use client'
 
+import { db } from "@/firebase";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react"
+import { useState } from "react";
+
 
 function CheckoutButton() {
 const {data:session} = useSession();
+const [loading, setLoading] = useState(false)
 
   const createCheckoutSession = async () => {
-      if(session) return;
+      if(!session?.user.id) return;
+
+      setLoading(true)
+
+      const docRef = await addDoc(
+        collection(db, 'customers', session.user.id, 'checkout_sessions'), {
+        price:'price_1OgiwJSAwBBmWXf8acP3A6Hy',
+        success_url: window.location.origin,
+        cancel_url: window.location.origin
+      })
+
+      return onSnapshot(docRef, snap => {
+        const data = snap.data();
+        const url = data?.url
+        const error = data?.error;
+
+
+        if (error){
+          // show an error to customer
+          // inspect your cloud function logs in your firebase console
+          alert(`An error occured: ${error.message}`);
+          setLoading(false);
+        }
+
+        if (url){
+          // we have a stripe checkout url, lets redirect
+          window.location.assign(url)
+          setLoading(false);
+        }
+      })
   }
 
   return (
     <div className='flex flex-col space-y-2'>
-   <button onClick={()=> createCheckoutSession()}  className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible-outline focus-visible-outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80">Sign Up</button>
+   <button onClick={()=> createCheckoutSession()}  className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible-outline focus-visible-outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80">
+    {loading ? "loading..." : "Sign Up"}
+    </button>
    </div>
   )
 }
