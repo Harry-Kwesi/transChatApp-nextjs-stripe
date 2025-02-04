e5fe9abafa8c86d617445fc33370bdac36ad1f11
Harry-Kwesi <harrydegraft@hotmail.com>
2025-02-04 10:18:07 +0530

    changed logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,6 @@ const Header = async () => {
       <nav className="flex flex-col sm:flex-row items-center  p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
         <div className="flex-1 flex items-center justify-end space-x-4">
-
         {session ? (
           <>
             <Link href={"/chat"} prefetch={false}>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import LogoImage from "../Images/Logos/log.svg";
-import { AspectRatio } from "./ui/aspect-ratio";
-import Image from 'next/image'
+// import LogoImage from "../Images/Logos/log.svg";
+// import { AspectRatio } from "./ui/aspect-ratio";
+// import Image from 'next/image'
+import { MessageSquare } from "lucide-react";
 
 const Logo = () => {
   return (
     <>
-      <Link href="/" prefetch={false} className="oveflow-hidden">
+      {/* <Link href="/" prefetch={false} className="oveflow-hidden">
       <div className="flex items-center w-52 h-4 mt-16 ">
       <AspectRatio ratio={14 / 9}
       className="flex items-center justify-between"
@@ -14,7 +15,11 @@ const Logo = () => {
      <Image priority src={LogoImage} alt="Logo" className="dark:filter dark:invert" /> 
   </AspectRatio>
         </div>
-        </Link>
+        </Link> */}
+          <div className="flex items-center">
+              <MessageSquare className="w-8 h-8 text-blue-600" />
+              <span className="ml-2 text-xl font-bold">TransChat</span>
+          </div>
     </>
   );
 };
