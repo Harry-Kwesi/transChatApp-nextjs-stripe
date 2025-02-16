1c4e5a6461e5272208277052b743e2035293f091
Harry-Kwesi <harrydegraft@hotmail.com>
2025-02-16 21:28:52 +0530

    completed the manage account button

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -14,6 +14,7 @@ import { signIn, signOut } from "next-auth/react";
 import { useSubscriptionStore } from "@/store/store";
 import LoadingSpinner from "./LoadingSpinner";
 import { StarIcon } from "lucide-react";
+import ManageAccountButton from "./ManageAccountButton";
 
 
 export default function UserButton({ session }: { session: Session | null }) {
@@ -56,7 +57,7 @@ export default function UserButton({ session }: { session: Session | null }) {
         )}
 
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Manage</DropdownMenuItem>
+        <DropdownMenuItem><ManageAccountButton/></DropdownMenuItem>
 
         <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
       </DropdownMenuContent>
